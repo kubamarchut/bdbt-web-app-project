3ac338098f1043c476b53be063c9d6e8d04da9f5
Jakub Marchut <kuba.marchut@gmail.com>
2022-12-29 13:44:49 +0100

    Updating dependencies for spring security

--- src/main/java/bdbt_projekt/SpringApplication/AppController.java
+++ src/main/java/bdbt_projekt/SpringApplication/AppController.java
@@ -1,6 +1,5 @@
 package bdbt_projekt.SpringApplication;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
